dcf6432cde0f99d2c4ab65a167dafe8f7f50ac77
chaeyeon111 <chaeyeon.park@outlook.com>
2020-02-21 18:41:09 +0900

    Update movies.jsx

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -9,25 +9,31 @@ class Movies extends Component {
         movies: getMovies()
     };
 
+    handleDelete= movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    }
+
     render() {
 
         //return table.table>thread>tr>th*4 ==> zen coding
         return <table className="table">
-            <thread>
+            <thead>
                 <tr>
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
                 </tr>
-            </thread>
+            </thead>
             <tbody>
             {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn.sm">Delete</button></td>
             </tr>
             ))}
             </tbody>
